4f440c79cd2866afc8fcac7d254c9dfd5342f7c2
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-20 16:17:23 +0100

    le plus beau task manager de l'univers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,12 +15,27 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save
     redirect_to task_path(@task)
-
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
